a51da6e3d1cd08069cdba29eef1e012b607d8be2
Daniel Ochoa <daniel@poeticsystems.com>
2014-11-29 15:44:57 -0600

    import libraries for transitions in fa-modifier.

--- app/components/fa-modifier.js
+++ app/components/fa-modifier.js
@@ -1,6 +1,20 @@
 import Ember from 'ember';
 import Modifier from 'famous/core/Modifier';
 import FamousComponent from './famous';
+import Transform from 'famous/core/Transform';
+
+/*
+ * Transitions
+ */
+import Transitionable from 'famous/transitions/Transitionable';
+import CachedMap from 'famous/transitions/CachedMap';
+import Easing from 'famous/transitions/Easing';
+import MultipleTransitions from 'famous/transitions/MultipleTransition';
+import SnapTransition from 'famous/transitions/SnapTransition';
+import SpringTransition from 'famous/transitions/SpringTransition';
+import TransitionableTransform from 'famous/transitions/TransitionableTransform';
+import TweenTransition from 'famous/transitions/TweenTransition';
+import WallTransition from 'famous/transitions/WallTransition';
 
 export default FamousComponent.extend({
   options: Ember.Object.create(),
